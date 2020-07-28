6b6cc327874394d82c2ff00671d8ca632474f210
Jim Denis <jimdenis007@yahoo.com>
2020-07-28 09:52:01 -0700

    Cleaned up JavaScript code

--- script.js
+++ script.js
@@ -1,85 +1,68 @@
 // Move info from the hour to local storage
 
 function store(hourPassed) {
-    var p1 = "Input"
-    var fullName = p1 + hourPassed
+    var p1 = "Input";
+    var fullName = p1 + hourPassed;
 
-    var p2 = "Hour"
-    var fullName2 = p2 + hourPassed
+    var p2 = "Hour";
+    var fullName2 = p2 + hourPassed;
     console.log("In store");
     console.log(hourPassed);
-    var notes = document.getElementById(fullName).value
+    var notes = document.getElementById(fullName).value;
     console.log(notes);
     localStorage.setItem(fullName2, notes);
 }
 
-
 //Shades the hours by past, current & future time...
 function doShade() {
-
     var p1 = "#";
     var p2 = "Input";
     var fullName = "";
-    
-    for (i=8; i < hh; i++) {
-        fullName = p1 +  p2 + i
+
+    for (i = 8; i < hh; i++) {
+        fullName = p1 + p2 + i;
         $(fullName).addClass("past");
     }
 
-    fullName = p1 +  p2 + hh
+    fullName = p1 + p2 + hh;
     $(fullName).addClass("present");
 
-    for (i = (hh + 1); i < 18; i++) {
-        fullName = p1 + p2 + i
+    for (i = hh + 1; i < 18; i++) {
+        fullName = p1 + p2 + i;
         $(fullName).addClass("future");
-    }    
+    }
 }
-
 
 //Goes to local storage & see if anything is there for our hours
 function doGet() {
+    var p1 = "Hour";
+    var fullName = "";
+    var p2 = "Input";
+    var fullName2 = "";
 
-  var p1 = "Hour";
-  var fullName = "";
-  var p2 = "Input";
-  var fullName2 = "";
-  
-  for (i=8; i < 18; i++) {
+    for (i = 8; i < 18; i++) {
+        fullName = p1 + i;
+        fullName2 = p2 + i;
+        var get = localStorage.getItem(fullName);
+        $(fullName).addClass("past");
 
-      fullName = p1 + i;
-      fullName2 = p2 + i;
-      var get = localStorage.getItem(fullName);
-      $(fullName).addClass("past");
-
-      if (get === null) {
-        continue; 
-      } else {
-        document.getElementById(fullName2).value = get;
-      } 
-
-  }
-  
+        if (get === null) {
+            continue;
+        } else {
+            document.getElementById(fullName2).value = get;
+        }
+    }
 }
-
-//Global Vars
-//var timeLeft = 9;
-//var iDone = false; 
-//var currIndex = -1
-//var numOfCorr = 0;
-//var numOfIncorr = 0;
-//var timeInterval = 0;
-//var gameOver = false;
-//var pharse = "Hello9";
 
 //PROGRAM STARTS HERE !!!!!!!!
 
 //Global Vars
 var d = new Date();
 var yyyy = d.getFullYear();
-var mm   = d.getMonth() + 1;
-var dd   = d.getDate();
-var hh   = d.getHours();
-var min  = d.getMinutes();
+var mm = d.getMonth() + 1;
+var dd = d.getDate();
+var hh = d.getHours();
+var min = d.getMinutes();
 
 //console.log(yyyy, mm, dd, hh, min);
 
@@ -91,50 +74,47 @@ document.getElementById("currentDay").textContent = dateTime;
 var get08 = localStorage.getItem("Hour08");
 document.getElementById("Input8").value = get08;
 
+doGet();
 
-doGet() 
-
-doShade()
-
+doShade();
 
 // Button listeners to start the process
 $("#Btn8").on("click", function () {
-  store(8)
+    store(8);
 });
 
 $("#Btn9").on("click", function () {
-  store(9)
+    store(9);
 });
 
 $("#Btn10").on("click", function () {
-  store(10)
+    store(10);
 });
 
 $("#Btn11").on("click", function () {
-  store(11)
+    store(11);
 });
 
 $("#Btn12").on("click", function () {
-  store(12)
+    store(12);
 });
 
 $("#Btn13").on("click", function () {
-  store(13)
+    store(13);
 });
 
 $("#Btn14").on("click", function () {
-  store(14)
+    store(14);
 });
 
 $("#Btn15").on("click", function () {
-  store(15)
+    store(15);
 });
 
 $("#Btn16").on("click", function () {
-  store(16)
+    store(16);
 });
 
 $("#Btn17").on("click", function () {
-  store(17)
+    store(17);
 });
-             
